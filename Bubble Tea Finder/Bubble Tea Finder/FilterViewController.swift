6315fdabac0cdd1b1404eb8872b6c0d57b95bd12
Alexander <Philalex196401@gmail.com>
2019-05-04 11:59:21 +0300

    Lesson4.1
    added filter for $, $$, $$$ & deals

--- Bubble Tea Finder/Bubble Tea Finder/FilterViewController.swift
+++ Bubble Tea Finder/Bubble Tea Finder/FilterViewController.swift
@@ -1,32 +1,8 @@
 /// Copyright (c) 2018 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import UIKit
+import CoreData
 
 class FilterViewController: UITableViewController {
 
@@ -50,11 +26,31 @@ class FilterViewController: UITableViewController {
   @IBOutlet weak var nameZASortCell: UITableViewCell!
   @IBOutlet weak var distanceSortCell: UITableViewCell!
   @IBOutlet weak var priceSortCell: UITableViewCell!
-
+  
+  var coreDataStack: CoreDataStack!
+  
+  lazy var cheapVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@", #keyPath(Venue.priceInfo.priceCategory), "$")
+  } ()
+  
+  lazy var moderateVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@",
+                       #keyPath(Venue.priceInfo.priceCategory), "$$")
+  } ()
+  
+  lazy var expebsiveVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@",
+                       #keyPath(Venue.priceInfo.priceCategory), "$$$")
+  }()
+  
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    populateCheapVenueCountLabel()
+    populateModerateVenueCountLabel()
+    populateExpensiveVenueCountLabel()
+    populateDealsCountLabel()
   }
 }
 
@@ -70,6 +66,87 @@ extension FilterViewController {
 extension FilterViewController {
 
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
   }
 }
+
+// MARK - Helper methods
+extension FilterViewController {
+  
+  func populateCheapVenueCountLabel() {
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = cheapVenuePredicate
+    do {
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first!.intValue
+      let pluralized = count == 1 ? "place" : "places"
+      firstPriceCategoryLabel.text = "\(count) bubble tea \(pluralized)"
+    } catch let error as NSError {
+      print("Count not fetch \(error), \(error.userInfo)")
+    }
+  }
+  
+  func populateModerateVenueCountLabel() {
+    
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = moderateVenuePredicate
+    
+    do {
+      let countResult =
+        try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first!.intValue
+      let pluralized = count == 1 ? "place" : "places"
+      secondPriceCategoryLabel.text =
+      "\(count) bubble tea \(pluralized)"
+    } catch let error as NSError {
+      print("Count not fetch \(error), \(error.userInfo)")
+    }
+  }
+  
+  func populateExpensiveVenueCountLabel() {
+    let fetchRequest: NSFetchRequest<Venue> = Venue.fetchRequest()
+    fetchRequest.predicate = expebsiveVenuePredicate
+    
+    do {
+      let count = try coreDataStack.managedContext.count(for: fetchRequest)
+      let pluralized = count == 1 ? "place" : "places"
+      thirdPriceCategoryLabel.text = "\(count) bubble tea \(pluralized)"
+    } catch let error as NSError {
+      print("Count not fetch \(error), \(error.userInfo)")
+    }
+  }
+  
+  func populateDealsCountLabel() {
+    
+    // 1
+    let fetchRequest = NSFetchRequest<NSDictionary>(entityName: "Venue")
+    fetchRequest.resultType = .dictionaryResultType
+    
+    // 2
+    let sumExpressionDesc = NSExpressionDescription()
+    sumExpressionDesc.name = "sumDeals"
+    
+    // 3
+    let specialCountExp = NSExpression(forKeyPath: #keyPath(Venue.specialCount))
+    sumExpressionDesc.expression = NSExpression(forFunction: "sum:", arguments: [specialCountExp])
+    sumExpressionDesc.expressionResultType = .integer32AttributeType
+    
+    // 4
+    fetchRequest.propertiesToFetch = [sumExpressionDesc]
+    
+    // 5
+    do {
+      let results = try coreDataStack.managedContext.fetch(fetchRequest)
+      
+      let resultDict = results.first!
+      let numDeals = resultDict["sumDeals"] as! Int
+      let pluralized = numDeals == 1 ? "deal" : "deals"
+      numDealsLabel.text = "\(numDeals) \(pluralized)"
+
+    } catch let error as NSError {
+      print("Could not fetch \(error), \(error.userInfo)")
+    }
+  }
+}
+
--- Bubble Tea Finder/Bubble Tea Finder/ViewController.swift
+++ Bubble Tea Finder/Bubble Tea Finder/ViewController.swift
@@ -1,32 +1,7 @@
 /// Copyright (c) 2018 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -35,6 +10,8 @@ class ViewController: UIViewController {
   private let venueCellIdentifier = "VenueCell"
 
   var coreDataStack: CoreDataStack!
+  var fetchRequest: NSFetchRequest<Venue>?
+  var venues: [Venue] = []
 
   // MARK: - IBOutlets
   @IBOutlet weak var tableView: UITableView!
@@ -42,13 +19,26 @@ class ViewController: UIViewController {
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    guard let model = coreDataStack.managedContext.persistentStoreCoordinator?.managedObjectModel,
+      let fetchRequest = model.fetchRequestTemplate(forName: "FetchRequest") as?NSFetchRequest<Venue> else {
+        return
+    }
+    
+    self.fetchRequest = fetchRequest
+    fetchAndReload()
   }
 
   // MARK: - Navigation
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == filterViewControllerSegueIdentifier {
-      
+    
+    guard segue.identifier == filterViewControllerSegueIdentifier,
+      let navController = segue.destination as? UINavigationController,
+      let filterVC = navController.topViewController as? FilterViewController else {
+        return
     }
+    
+    filterVC.coreDataStack = coreDataStack
   }
 }
 
@@ -59,17 +49,35 @@ extension ViewController {
   }
 }
 
+// MARK: - Helper methods
+extension ViewController {
+  func fetchAndReload() {
+    
+    guard let fetchRequest = fetchRequest else {
+      return
+    }
+    
+    do {
+      venues = try coreDataStack.managedContext.fetch(fetchRequest)
+      tableView.reloadData()
+    } catch let error as NSError {
+      print("Could not fetch \(error), \(error.userInfo)")
+    }
+  }
+}
+
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return venues.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: venueCellIdentifier, for: indexPath)
-    cell.textLabel?.text = "Bubble Tea Venue"
-    cell.detailTextLabel?.text = "Price Info"
+    let venue = venues[indexPath.row]
+    cell.textLabel?.text = venue.name
+    cell.detailTextLabel?.text = venue.priceInfo?.priceCategory
     return cell
   }
 }
